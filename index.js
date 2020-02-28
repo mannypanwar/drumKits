14f3c3ffcc9bce4fda21fa64c44907211fb5ba96
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-02-28 21:07:32 +0530

    added sound on keypress

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-for (i = 0; i <= 6; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
-}
-function handleClick() {
-  var sound = this.innerHTML;
+//detecting keyboard press
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+});
 
-  switch (sound) {
+//detecting button press
+for (i = 0; i <= 6; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var sound = this.innerHTML;
+    makeSound(sound);
+  });
+}
+
+//function to make sound
+function makeSound(key) {
+  switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
@@ -38,29 +47,4 @@ function handleClick() {
       console.log(sound);
       break;
   }
-
-  //code in if statements
-
-  // if (this.innerHTML === "w") {
-  //   var audio = new Audio("sounds/tom-1.mp3");
-  //   audio.play();
-  // } else if (this.innerHTML === "a") {
-  //   var audio = new Audio("sounds/tom-2.mp3");
-  //   audio.play();
-  // } else if (this.innerHTML === "s") {
-  //   var audio = new Audio("sounds/tom-3.mp3");
-  //   audio.play();
-  // } else if (this.innerHTML === "d") {
-  //   var audio = new Audio("sounds/tom-4.mp3");
-  //   audio.play();
-  // } else if (this.innerHTML === "j") {
-  //   var audio = new Audio("sounds/snare.mp3");
-  //   audio.play();
-  // } else if (this.innerHTML === "k") {
-  //   var audio = new Audio("sounds/crash.mp3");
-  //   audio.play();
-  // } else if (this.innerHTML === "l") {
-  //   var audio = new Audio("sounds/kick-bass.mp3");
-  //   audio.play();
-  // }
 }
